11e24b80321eac64f931c437612e818cb84ce0c8
Ste Griffiths <github@stegriff.co.uk>
2021-01-28 20:26:54 +0000

    Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormTestComponent } from './form-test/form-test.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'form-twins', component: FormTestComponent },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormTestComponent
+    FormTestComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
